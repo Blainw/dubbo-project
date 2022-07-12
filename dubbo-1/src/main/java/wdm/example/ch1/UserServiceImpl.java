d9576a27788448be875952b5a7b11a8751d4af02
wdm <1831486177@qq.com>
2022-07-12 17:56:08 +0800

    dubbo-project 2022-07-12 times 2

--- dubbo-1/src/main/java/wdm/example/ch1/UserServiceImpl.java
+++ dubbo-1/src/main/java/wdm/example/ch1/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package wdm.example.ch1;
 
+import org.springframework.stereotype.Service;
+
 public class UserServiceImpl implements UserService {
     public User queryUserById(String name) {
         return new User(name,23);
